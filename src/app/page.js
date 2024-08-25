5671edc0d8e07d56810502665e88e5936a9f8862
Abhishek <abhi.jkit@gmail.com>
2024-08-25 14:26:57 +0530

    Poftfolio website design work

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import Stats from "./components/stats";
 
 export default function Home() {
   return (
+  
     <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
