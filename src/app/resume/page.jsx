528ae04ecca14a5fc29151240305f6671c9fdec4
Abhishek <abhi.jkit@gmail.com>
2024-08-26 16:04:22 +0530

    Poftfolio website design work

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -125,6 +125,7 @@ export default function Resume() {
           <TabsContent value="abot">
               advadv
           </TabsContent >
+          
         </Tabs>
       </div>
     </section>
